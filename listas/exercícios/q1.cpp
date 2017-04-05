8cd5783ebccd770ff412bfaa5fff4fc44d732af4
alyssonmelo <aly.melo@outlook.com>
2017-04-05 17:27:43 -0300

    solução1

---
+++ listas/exercícios/q1.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+printf("insira um numero inteiro \n");
+int valor;
+scanf("%d",&valor);
+printf("O valor: %d %d \n",valor,3);
+system("pause");
+}
+
